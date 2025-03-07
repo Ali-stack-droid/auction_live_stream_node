194f4836e276030494ac2634c241029d443d22dd
umer <umar.darazkhan4466@gmail.com>
2025-03-07 23:03:22 +0500

    call generated with generatecalltoken

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 app.use(express.json());
 
 const { StreamClient } = require("@stream-io/node-sdk");
+const axios = require("axios");
 
 const apiKey = "k532vzf4a7cx";
 const secret = "nepaw7eakf3ddb87v6xbezs9dtaq8rjaxwhsypjwb6ewmvd6apb9mfmczpwy3rrm";
-
+const BASE_URL = `https://auction.sttoro.com/api`
 const client = new StreamClient(apiKey, secret);
 
 // const fetchStreamToken = async (userId) => {
@@ -24,39 +25,57 @@ const client = new StreamClient(apiKey, secret);
 
 app.post("/initialize-stream", async (req, res) => {
     try {
-        const { userId, callId } = req.body;
-        if (!userId || !callId) {
-            return res.status(400).json({ error: "User ID and Call ID are required" });
+        const { userId, callId, lotID } = req.body;
+        if (!userId && !callId && !lotID) {
+            return res.status(400).json({ error: `${!userId && `User ID`}${!callId && `Call ID`}${!lotID && `Lot ID`} are required` });
         }
 
         const newUser = {
             id: userId,
             role: "admin",
             custom: { color: "red" },
-            name: "John",
+            name: "Admin",
         };
         await client.upsertUsers([newUser]);
 
-        const token = await client.generateUserToken(userId);
 
         const callType = "default";
         const call = client.video.call(callType, callId);
-        await call.create({
+        const callCreated = await call.create({
             data: {
                 created_by_id: userId,
                 members: [{ user_id: userId, role: "admin" }],
                 custom: { color: "blue" },
             },
         });
+        const vailidity = 60 * 60;
+        const token = client.generateUserToken({ user_id: userId, validity_in_seconds: vailidity });
 
-        res.json({ token, callId, callType });
+        let payload = {
+            LotId: lotID,
+            Token: token,
+            CallId: callId,
+            UserId: userId
+        }
+
+        console.log(payload);
+
+
+        const response = axios.post(`https://auction.sttoro.com/api/stream/create`, payload, {
+
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        console.log("stream/create api response ", JSON.stringify(response, null, 2));
+        res.json({ token, callId, callType, callCreated });
     } catch (error) {
         console.error("Error:", error);
         res.status(500).json({ error: "Failed to initialize stream" });
     }
 });
 
-const PORT = 3333;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`⁠Server running on port ${PORT}`);
 });
